1079718584f6718c384035334c681c09154db840
Christo Bezuidenhout <bez.christo@gmail.com>
2020-05-09 14:42:22 +0200

    updated subscribe function

--- src/functions/subscribe/index.js
+++ src/functions/subscribe/index.js
@@ -1,26 +1,27 @@
 "use strict";
 
-const Firestore = require('@google-cloud/firestore');
+const Firestore = require("@google-cloud/firestore");
 const PROJECTID = process.env.GCLOUD_PROJECT;
 
-const COLLECTION_NAME = 'transactions';
+const COLLECTION_NAME = "transactions";
 const firestore = new Firestore({
   projectId: PROJECTID,
   timestampsInSnapshots: true,
 });
 
-exports.subscribe = (pubsubMessage) => {  
-    console.log(pubsubMessage);
-    // store/insert a new document
-    const data = JSON.parse(Buffer.from(pubsubMessage.data, "base64").toString()) || {};    
-    const created = new Date().getTime();
-    return firestore.collection(COLLECTION_NAME)
-      .add({ created, data })
-      .then(doc => {
-        console.log("success", doc);
-      }).catch(err => {
-        console.error(err);
-        console.log(error, err);
-      });
-  
-}
+exports.subscribe = (pubsubMessage) => {
+  // store/insert a new document
+  const data =
+    JSON.parse(Buffer.from(pubsubMessage.data, "base64").toString()) || {};
+  const created = new Date().getTime();
+  return firestore
+    .collection(COLLECTION_NAME)
+    .add({ created, ...data })
+    .then((doc) => {
+      console.log("success", doc);
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log(error, err);
+    });
+};
